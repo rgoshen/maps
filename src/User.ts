d9a544e99c1892bd54baad714eb1d1b3f7e529d0
Richard Goshen <rick.goshen@gmail.com>
2023-02-18 12:14:16 -0700

    add data to create a random user

--- src/User.ts
+++ src/User.ts
@@ -1,17 +1,15 @@
-import { randLatitude, randLongitude, randFullName } from '@ngneat/falso';
-
-class User {
+export class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
 
-  constructor() {
-    this.name = randFullName();
+  constructor(name: string, lat: number, lng: number) {
+    this.name = name;
     this.location = {
-      lat: randLatitude(),
-      lng: randLongitude(),
+      lat,
+      lng,
     };
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('Hi there!');
+import { randLatitude, randLongitude, randFullName } from '@ngneat/falso';
+import { User } from './User';
+
+const user = new User(randFullName(), randLatitude(), randLongitude());
+console.log(user);
