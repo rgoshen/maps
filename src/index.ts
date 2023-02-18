33340396745e79a7fd7cda19344a72bfa676ed3e
Richard Goshen <rick.goshen@gmail.com>
2023-02-18 14:57:05 -0700

    add initial google map to project

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,43 @@ import {
   randCompanyName,
   randCatchPhrase,
 } from '@ngneat/falso';
-import { Company } from './Company';
-import { User } from './User';
+// import { Company } from './Company';
+// import { User } from './User';
 
-const user = new User(randFullName(), randLatitude(), randLongitude());
-const company = new Company(
-  randCompanyName(),
-  randCatchPhrase(),
-  randLatitude(),
-  randLongitude()
-);
-console.log(user, company);
+// const user = new User(randFullName(), randLatitude(), randLongitude());
+// const company = new Company(
+//   randCompanyName(),
+//   randCatchPhrase(),
+//   randLatitude(),
+//   randLongitude()
+// );
+// console.log(user);
+// console.log(company);
+
+// Initialize and add the map
+function initMap(): void {
+  // Randomly generated coord
+  const coord = { lat: randLatitude(), lng: randLongitude() };
+
+  const mapDiv = document.getElementById('map');
+
+  // The map, centered at coord
+  const map = new google.maps.Map(mapDiv as HTMLElement, {
+    zoom: 1,
+    center: coord,
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: coord,
+    map: map,
+  });
+  console.log(coord);
+}
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+window.initMap = initMap;
