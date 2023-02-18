c209f2670f1d821dfc0b18a6c76c036e6abb6de3
Richard Goshen <rick.goshen@gmail.com>
2023-02-18 12:25:23 -0700

    add company entity

--- src/Company.ts
+++ src/Company.ts
@@ -1,12 +1,14 @@
 export class Company {
   name: string;
+  catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
 
-  constructor(name: string, lat: number, lng: number) {
+  constructor(name: string, catchPhrase: string, lat: number, lng: number) {
     this.name = name;
+    this.catchPhrase = catchPhrase;
     this.location = {
       lat,
       lng,
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@ import {
   randLongitude,
   randFullName,
   randCompanyName,
+  randCatchPhrase,
 } from '@ngneat/falso';
 import { Company } from './Company';
 import { User } from './User';
 
 const user = new User(randFullName(), randLatitude(), randLongitude());
-const company = new Company(randCompanyName(), randLatitude(), randLongitude());
+const company = new Company(
+  randCompanyName(),
+  randCatchPhrase(),
+  randLatitude(),
+  randLongitude()
+);
 console.log(user, company);
